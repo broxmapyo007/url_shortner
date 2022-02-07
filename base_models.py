e1443635438defa39f72e031e718e32713f4ebf6
Adesh Dangi <adeshdangi104@gmail.com>
2022-02-07 08:42:13 +0530

    Update base_models.py

--- base_models.py
+++ base_models.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, EmailStr
 from typing import List, Optional, Dict
 
 class url_req(BaseModel):
-    original_url : str = "https://www.google.com"
+    original_url : str = "" #"https://www.google.com"
     special_key : Optional[str] = ""
     message : Optional[list] = []
     email : EmailStr #= ""
